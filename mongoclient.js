3cb912c709fa162304f96652aed2bdb593060f50
SchmandWorks <ykurtz@htwsaar.de>
2024-10-13 17:44:38 +0200

    Update mongoclient.js

--- mongoclient.js
+++ mongoclient.js
@@ -5,5 +5,5 @@ const client = new MongoClient(process.env.MONGODB_URI);
 
 export async function connectToDatabase() {
   await client.connect();
-  return client.db('Dexscreener'); // Specify the database name here
+  return client.db('Raydium'); // Specify the database name here
 }
